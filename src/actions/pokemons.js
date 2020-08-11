017968ae4ac7facb11ff560701a2a72c4006f332
Shubham Singh <shubham88singh@gmail.com>
2020-08-12 04:05:53 +0530

    Setting up the initial redux details

---
+++ src/actions/pokemons.js
@@ -0,0 +1,5 @@
+export const GET_POKEMONS='GET_POKEMONS';
+
+export const GET_POKEMON='GET_POKEMON';
+export const RESET_POKEMON='RESET_POKEMON';
+
---
+++ src/reducers/index.js
@@ -0,0 +1,3 @@
+import { combineReducers } from 'redux';
+import pokemons from './pokemons';
+export default combineReducers({ pokemons });

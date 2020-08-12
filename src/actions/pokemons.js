67b20df040f827892d32de834e9f77c683a9419b
Shubham Singh <shubham88singh@gmail.com>
2020-08-12 06:47:17 +0530

    Adding the thunk middleware

--- src/actions/pokemons.js
+++ src/actions/pokemons.js
@@ -3,3 +3,10 @@ export const GET_POKEMONS='GET_POKEMONS';
 export const GET_POKEMON='GET_POKEMON';
 export const RESET_POKEMON='RESET_POKEMON';
 
+
+
+export function getPokemons(){
+    return async function(dispatch){
+        const res=await fetch()
+    }
+}
--- src/store.js
+++ src/store.js
@@ -1,7 +1,14 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, {}, composeWithDevTools());
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  {},
+  composeWithDevTools(applyMiddleware(...middleware))
+);
 
 export default store;

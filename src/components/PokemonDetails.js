dba637618f74e417cef093b12e550f089dec228a
Shubham Singh <shubham88singh@gmail.com>
2020-08-13 08:58:48 +0530

    Adding the link to evolution phase

--- src/components/PokemonDetails.js
+++ src/components/PokemonDetails.js
@@ -57,7 +57,7 @@ const PokemonDetails = () => {
           </div>
         </div>
         <div className="text-lg    m-2  ">
-          <div className="mt-2 mr-2 text-4xl">Type </div>
+          <div className="my-4  text-4xl">Type </div>
           <div className="flex">
             {pokemon.types.map((type) => (
               <span
@@ -70,9 +70,9 @@ const PokemonDetails = () => {
             ))}
           </div>
         </div>
-        <div className="px-4 py-6">
-          <div className="text-4xl">Moves</div>
-          <div className="my-2 flex  flex-wrap m-2 px-4 py-2 ">
+        <div className="text-4xl">Moves</div>
+        <div className="py-6">
+          <div className="my-2 flex  flex-wrap  px-4 py-2 ">
             {pokemon.moves.map((pokemon) => (
               <div className="px-4 py-2 bg-gray-200 m-2 rounded-lg text-gray-600">
                 {pokemon.move.name}
@@ -81,21 +81,30 @@ const PokemonDetails = () => {
           </div>
         </div>
         <div className="text-4xl">Evolution</div>
-        <div className="w-full bg-gray-400 px-8 py-6 flex">
+        <div className=" bg-gray-200 p-4  my-2 w-full justify-between items-center rounded-lg  flex">
           {evolutions.map((evolution) => {
             return (
-              <div className="flex flex-col">
+              
+              <div className="flex flex-col p-8 ">
+                <Link to={`${evolution.id}`}>
                 <img
+                  className="w-64 rounded-full p-4 border border-black  "
                   src={`https://pokeres.bastionbot.org/images/pokemon/${evolution.id}.png`}
                   alt={evolution.name}
                 />
-                <h2 className="text-3xl">{evolution.name}</h2>
+                </Link>
+                <h2 className="text-3xl capitalize">{evolution.name}</h2>
               </div>
+              
             );
           })}
         </div>
         <Link to="/">
-          <button>Explore more Pokemon</button>
+          <div className="flex justify-center my-4">
+            <button className="px-4 py-2 bg-purple-700 text-purple-100  rounded-lg shadow-lg focus:outline-none hover:bg-purple-600 ">
+              Explore more Pokemon
+            </button>
+          </div>
         </Link>
       </main>
     </div>

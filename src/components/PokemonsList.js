00cf0f7e8c08bc49d4e6e02921162ee39b4d1985
Shubham Singh <shubham88singh@gmail.com>
2020-08-13 22:39:39 +0530

    Adding the filter option

--- src/components/PokemonsList.js
+++ src/components/PokemonsList.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPokemons } from '../actions';
-
 
 import PokemonCard from './PokemonCard';
 const PokemonsList = () => {
@@ -9,16 +8,77 @@ const PokemonsList = () => {
   useEffect(() => {
     dispatch(getPokemons());
   }, [dispatch]);
+  const pokeTypes = [
+    'normal',
+    'fire',
+    'water',
+    'grass',
+    'electric',
+    'ice',
+    'fighting',
+    'poison',
+    'ground',
+    'psychic',
+    'bug',
+    'rock',
+    'ghost',
+    'dark',
+    'dragon',
+    'steel',
+    'fairy',
+  ];
+
+  const [type, setType] = useState('All');
+
   const pokemons = useSelector((state) => state.data.pokemons);
   const isLoaded = useSelector((state) => state.data.isPokemonsLoaded);
+  const filteredPokemon = useMemo(
+    () =>
+      type !== 'All'
+        ? pokemons.filter((pokemon) => {
+            let arr = pokemon.types;
+
+            for (let i = 0; i < arr.length; i++) {
+              if (arr[i].type.name === type) return true;
+            }
+            return false;
+          })
+        : pokemons,
+    [type, pokemons]
+  );
+  console.log(filteredPokemon, type);
   return (
-    <div className="my-12 flex flex-wrap">
-      {isLoaded ? (
-        pokemons.map((pokemon) => <PokemonCard pokemon={pokemon} />)
-      ) : (
-        <div>Loading</div>
-      )}
-    </div>
+    <>
+      <select
+        name="filter"
+        value={type}
+        onChange={(e) => setType(e.target.value)}
+        id="filter"
+      >
+        <option value="All" defaultValue>
+          All
+        </option>
+        {pokeTypes.map((type) => (
+          <option key={type} value={type}>
+            {type}
+          </option>
+        ))}
+      </select>
+      <div className="my-12 flex flex-wrap">
+        {isLoaded ? (
+          filteredPokemon.map((pokemon) => <PokemonCard pokemon={pokemon} />)
+        ) : (
+          <lottie-player
+            src="https://assets4.lottiefiles.com/temp/lf20_Tw0dyZ.json"
+            background="transparent"
+            speed="1"
+            loop
+            className="w-64 h-64 mx-auto"
+            autoplay
+          ></lottie-player>
+        )}
+      </div>
+    </>
   );
 };
 
